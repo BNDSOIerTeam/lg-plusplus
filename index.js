d6b583bf31dc9ccf65b0573bed3a947fbfbe56db
ZhongTianrui <106294609+ZhongTianrui@users.noreply.github.com>
2024-04-10 21:04:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,15 @@
 (function() {
     'use strict';
     var _nav = document.querySelectorAll("[data-v-258e49ac]")[0];
-    var Flag = document.createElement("div")
-    Flag.innerHTML = "<a class=\"am-btn am-btn-success am-btn-sm\" style=\"border-radius: 5px; font-size: 13px; margin-bottom: 0px; padding: 0.5em 1em; font-weight: 400; line-height: 1.2; text-align: center; white-space: nowrap; background: green; border: 1px solid transparent; cursor: pointer; outline: 0px; appearance: none; user-select: none; transition: background-color 300ms ease-out 0s, border-color 300ms ease-out 0s; margin-right: 10px; box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px; color: white;\"><b>LG++++</b></a>";
-    _nav.appendChild(Flag);
+    var Flag = document.createElement("a")
+    Flag.style="border-radius: 5px; font-size: 13px; margin-bottom: 0px; padding: 0.5em 1em; font-weight: 400; line-height: 1.2; text-align: center; white-space: nowrap; background: green; border: 1px solid transparent; cursor: pointer; outline: 0px; appearance: none; user-select: none; transition: background-color 300ms ease-out 0s, border-color 300ms ease-out 0s; margin-right: 10px; box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px; color: white;";
+    var _ = document.createElement("b");
+    _.innerHTML="Luogu++";
+    Flag.appendChild(_);
+    var divv = document.createElement("div")
+    divv.appendChild(Flag);
+//    Flag.innerHTML = "<a class=\"am-btn am-btn-success am-btn-sm\" style=\"border-radius: 5px; font-size: 13px; margin-bottom: 0px; padding: 0.5em 1em; font-weight: 400; line-height: 1.2; text-align: center; white-space: nowrap; background: green; border: 1px solid transparent; cursor: pointer; outline: 0px; appearance: none; user-select: none; transition: background-color 300ms ease-out 0s, border-color 300ms ease-out 0s; margin-right: 10px; box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px; color: white;\"><b>LG++++</b></a>";
+    _nav.innerHTML = divv.innerHTML + _nav.innerHTML;
     console.log("[lg++] load success!");
     // Your code here...
 })();
